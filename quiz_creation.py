cd39a9bad027b2f313fdedf1e8fbe224246eb29c
Lou <marilounacional0717@gmail.com>
2025-04-23 21:06:34 +0800

    welcome msg

--- quiz_creation.py
+++ quiz_creation.py
@@ -1,4 +1,8 @@
 # make a welcome message where it is shown at the first 
+print("Lulu's Quiz Maker")    
+print("Please input your questions!")
+print("Enter 'exit' if done\n")
+
 # user inputs the questions
 # the user inputs the choices
 # inputs the right answer from the choices
